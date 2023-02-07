faef1630745d6d8356c767e1b79eb7def79c962f
julio <julio.minellamatheus@gmail.com>
2023-02-06 23:03:57 -0300

    feat: response html with go struct

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      int
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -14,4 +21,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 1},
+		{"tenis", "Confortável", 100, 3},
+		{"fone", "muito bom", 59, 2},
+	}
+
+	temp.ExecuteTemplate(w, "index", produtos)
 }
